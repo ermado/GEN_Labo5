bcdfe564f1d58e3707c5af915d4d6a555a6e05cf
Edoardo Carpita <edoardo.carpita@depsys.ch>
2019-05-24 10:33:41 +0200

    first push

---
+++ cmake-build-debug/CMakeFiles/GEN_Labo5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GEN_Labo5.dir/main.cpp.obj"
+  "GEN_Labo5.pdb"
+  "GEN_Labo5.exe"
+  "GEN_Labo5.exe.manifest"
+  "libGEN_Labo5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GEN_Labo5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/GEN_Labo5.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GEN_Labo5.
+# This may be replaced when dependencies are built.
