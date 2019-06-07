b6070d223ae50b59c9998027def972948f22b762
Marion Dutu <marion.dutulaunay@heig-vd.ch>
2019-06-07 11:39:35 +0200

    wii

--- Customer.h
+++ Customer.h
@@ -3,6 +3,7 @@
 #define CUSTOMER_H
 #include <string>
 #include <vector>
+#include <sstream>
 #include "Rental.h"
 
 class Customer {
@@ -31,5 +32,52 @@ addRental( const Rental& arg ) { _rentals.push_back( arg ); }
 
 inline std::string Customer::
 getName() const { return _name; }
+
+/* inline std::string Customer::statement()
+{
+    double totalAmount = 0;
+    int frequentRenterPoints = 0;
+    std::vector< Rental >::iterator iter = _rentals.begin();
+    std::vector< Rental >::iterator iter_end = _rentals.end();
+    std::ostringstream result;
+    result << "Rental Record for " << getName() << "\n";
+    for ( ; iter != iter_end; ++iter ) {
+        double thisAmount = 0;
+        Rental each = *iter;
+
+        // determine amounts for each line
+        switch ( each.getMovie().getPriceCode() ) {
+            case Movie::REGULAR:
+                thisAmount += 2;
+                if ( each.getDaysRented() > 2 )
+                    thisAmount += ( each.getDaysRented() - 2 ) * 1.5 ;
+                break;
+            case Movie::NEW_RELEASE:
+                thisAmount += each.getDaysRented() * 3;
+                break;
+            case Movie::CHILDRENS:
+                thisAmount += 1.5;
+                if ( each.getDaysRented() > 3 )
+                    thisAmount += ( each.getDaysRented() - 3 ) * 1.5;
+                break;
+        }
+
+        // add frequent renter points
+        frequentRenterPoints++;
+        // add bonus for a two day new release rental
+        if ( ( each.getMovie().getPriceCode() == Movie::NEW_RELEASE )
+             && each.getDaysRented() > 1 ) frequentRenterPoints++;
+
+        // show figures for this rental
+        result << "\t" << each.getMovie().getTitle() << "\t"
+               << thisAmount << "\n";
+        totalAmount += thisAmount;
+    }
+    // add footer lines
+    result << "Amount owed is " << totalAmount << "\n";
+    result << "You earned " << frequentRenterPoints
+           << " frequent renter points";
+    return result.str();
+} */
 
 #endif // CUSTOMER_H
--- MockMovie.h
+++ MockMovie.h
@@ -13,6 +13,8 @@ class MockMovie : public Movie {
 public:
 
     MOCK_METHOD0(getTitle, const std::string());
+    MOCK_METHOD0(getPriceCode, int());
+    MOCK_METHOD1(setPriceCode, void(int price));
 
 };
 
