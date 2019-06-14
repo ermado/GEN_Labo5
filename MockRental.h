8120404b44fec7ba51f2a17cc46d96d9ebf67c48
Edoardo Carpita <edoardo.carpita@depsys.ch>
2019-06-14 10:45:07 +0200

    add mock rental

---
+++ MockRental.h
@@ -0,0 +1,19 @@
+//
+// Created by Edd993Surface on 07/06/2019.
+//
+
+#ifndef GEN_LABO5_MOCKRENTAL_H
+#define GEN_LABO5_MOCKRENTAL_H
+
+#include "gmock/gmock.h"
+#include "rental.h"
+
+class MockRental : public Rental {
+public:
+
+    MOCK_CONST_METHOD0(getMovie, const Movie&());
+    MOCK_CONST_METHOD0(getDaysRented(), int());
+
+};
+
+#endif //GEN_LABO5_MOCKRENTAL_H
