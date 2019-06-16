efacecd2014e344484e40ceb1f4e33d3762c8510
Edoardo Carpita <edoardo.carpita@depsys.ch>
2019-06-16 17:17:46 +0200

    mocking of movie classes

---
+++ MockMovie.h
@@ -0,0 +1,37 @@
+//
+// Created by Edd993Surface on 16/06/2019.
+//
+
+#ifndef GEN_LABO5_MOCKMOVIE_H
+#define GEN_LABO5_MOCKMOVIE_H
+
+class MockRegularMovie : public Movie {
+public:
+
+    MOCK_CONST_METHOD1(getAmount, double);
+    MOCK_CONST_METHOD0(getRenterPoints, int);
+
+};
+
+class MockNewReleaseMovie : public Movie {
+public:
+
+    MOCK_CONST_METHOD1(getAmount, double);
+    MOCK_CONST_METHOD0(getRenterPoints, int);
+
+};
+
+class MockChildrenMovie : public Movie {
+public:
+
+    MOCK_CONST_METHOD1(getAmount, double);
+    MOCK_CONST_METHOD0(getRenterPoints, int);
+
+};
+
+
+
+
+
+
+#endif //GEN_LABO5_MOCKMOVIE_H
